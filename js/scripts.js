cc0f4baf8b780b616e41429419ce641fc7c2e753
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-30 11:48:52 +0300

    Gave ids to the DESIGN, DEVELOPMENT and PRODUCT MANAGEMENT and also a click function in JS to show and hide the paragraph

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#clickable").click(function () {
+        $("#DESIGN").toggle();
+    })
+    $("#clickable-dev").click(function () {
+        $("#dev").toggle();
+    })
+    $("#clickable-pro").click(function () {
+        $("#pro").toggle();
+    })
+})
+
